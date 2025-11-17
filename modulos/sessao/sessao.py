dac9d632e3e9b91f1cb106f084c17994ede3f16a
yurits19 <yurits19@gmail.com>
2025-11-17 19:15:45 -0300

    implementa funções busca_sessao,assentos_disponiveis e reserva_asssento

--- modulos/sessao/sessao.py
+++ modulos/sessao/sessao.py
@@ -63,3 +63,64 @@ def cria_sessao (filme_id, sala, horario, capacidade, formato_exibicao) -> int:
     listaSessoes.append(nova_sessao)
     
     return padrao_retornos.SUCESSO
+
+
+
+def busca_sessao(sessao_id: int) -> dict | None:
+    """
+    Busca uma sessão específica na listaSessoes pelo seu ID.
+    """
+    if not isinstance(sessao_id, int) or sessao_id <= 0:
+        return None
+        
+    for sessao in listaSessoes:
+        if sessao["id"] == sessao_id:
+            return sessao 
+            
+    return None  
+
+
+def assentos_disponiveis(sessao_id:int) -> int:
+    sessao_encontrada = busca_sessao(sessao_id)
+
+    if sessao_encontrada is None:
+        return -1
+    capacidade_total = sessao_encontrada["capacidade"]
+    capacidade_atual = capacidade_total - len(sessao_encontrada["assentos_ocupados"])
+    return capacidade_atual
+
+
+ERRO_SESSAO_LOTADA = 5
+
+def reserva_assento(sessao_id: int, numero_assento: int) -> int:
+    """
+    Reserva um assento específico em uma sessão.
+    Retorna um código de status inteiro.
+    """
+    if not isinstance(sessao_id, int) or sessao_id <= 0:
+        return padrao_retornos.PARAMETRO_INVALIDO
+    
+    if not isinstance(numero_assento, int) or numero_assento <= 0:
+        return padrao_retornos.PARAMETRO_INVALIDO
+
+    sessao_encontrada = busca_sessao(sessao_id)
+    
+    if sessao_encontrada is None:
+        return padrao_retornos.NAO_ENCONTRADO 
+
+    capacidade_total = sessao_encontrada["capacidade"]
+    if numero_assento > capacidade_total:
+        return padrao_retornos.PARAMETRO_INVALIDO 
+
+    lista_de_ocupados = sessao_encontrada["assentos_ocupados"]
+    
+    if numero_assento in lista_de_ocupados:
+        return padrao_retornos.JA_EXISTE 
+
+    if len(lista_de_ocupados) >= capacidade_total:
+        return ERRO_SESSAO_LOTADA 
+    # Sucesso
+    lista_de_ocupados.append(numero_assento)
+    
+    return padrao_retornos.SUCESSO 
+
