c53e9cf13bf0744a2d028159377e0a4748f34055
fernanda.auler <fernandaauler1@gmail.com>
2025-11-23 00:40:51 -0300

    ajeitei o import de filme dentro do modulo

--- modulos/filme/filme_test.py
+++ modulos/filme/filme_test.py
@@ -1,4 +1,4 @@
-import filme
+import modulos.filme.filme as filme
 
 def test_criaFilme():
     filmeCriadoComSucesso = filme.criaFilme("Interstellar", "As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar.", "Ficção Científica", 169.0, 10, "2014-11-06")
